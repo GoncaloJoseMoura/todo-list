d354303fe8dbc58093b3818718d564ebc438c736
GoncaloJoseMoura <43656133+GoncaloJoseMoura@users.noreply.github.com>
2024-05-14 17:44:33 +0100

    Add Favicon Path

--- webpack.common.js
+++ webpack.common.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      favicon: "./src/assets/favicon.svg",
       filename: 'index.html',
       inject: 'head',
       scriptLoading: 'defer',
