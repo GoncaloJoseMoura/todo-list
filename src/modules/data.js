d962f687ecfbe40336bf75537a8fb35504efbd65
GoncaloJoseMoura <43656133+GoncaloJoseMoura@users.noreply.github.com>
2024-05-18 23:17:44 +0100

    Add Object Features

---
+++ src/modules/data.js
@@ -0,0 +1,43 @@
+
+const Storage = (function() {
+
+    let task_storage = {}
+
+    const addTask = (element, project) => {
+
+        if (!(task_storage[project].some(elem => {
+            return elem.text == element.text
+        }))) {
+            task_storage[project].push(element)
+        }
+    }
+
+    const createProject = (name) => {
+
+        if (!(name in task_storage)) {
+            task_storage[name] = []
+        } 
+    }
+
+    const deleteProject = (name) => {
+        delete task_storage[name]
+
+    }
+
+    const deleteTask = (task, project) => {
+        const check = task_storage[project].some((element) => element.text == task.text)
+        if (check) {
+            task_storage[project].splice([check], 1)
+        }
+    }
+
+    return {
+        addTask,
+        createProject,
+        deleteProject,
+        deleteTask,
+        task_storage
+    }
+})()
+
+export default Storage
